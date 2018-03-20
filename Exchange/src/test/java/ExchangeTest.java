eb1f89757ebf51826aaba458064bdd391aa01d22
Nikolai Bystrov <nikolai_bystrov@epam.com>
2018-03-20 12:35:29 +0300

    Added Jackson

--- Exchange/src/test/java/ExchangeTest.java
+++ Exchange/src/test/java/ExchangeTest.java
@@ -1,5 +1,8 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -10,11 +13,16 @@ public class ExchangeTest {
 
   @Test
   public void name() throws IOException {
-    URL url = new URL("https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start=1521400000&end9999999999&period=300");
+    URL url = new URL(
+        "https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start=1521400000&end9999999999&period=300");
     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    urlConnection.connect();
     System.out.println(urlConnection.getResponseMessage());
-    try(BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))){
-      System.out.println(reader.readLine());
+    System.out.println();
+    try (InputStream inputStream = urlConnection.getInputStream()) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      JsonNode node = objectMapper.readTree(inputStream);
+      System.out.println(node);
     }
     urlConnection.disconnect();
   }
