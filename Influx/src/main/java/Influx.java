5f54c14c3ec60f4ae00e8b1c32e6f9fca53d9d52
Nikolai Bystrov <nikolai_bystrov@epam.com>
2018-03-22 16:10:51 +0300

    Created and configured docker.yml file for postgres database

--- Influx/src/main/java/Influx.java
+++ Influx/src/main/java/Influx.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.BatchPoints;
@@ -5,38 +8,41 @@ import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
 
-import java.io.*;
+
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Influx {
-    public static void main(String[] args) throws IOException {
-        InfluxDB influxDB = InfluxDBFactory.connect("http://127.0.0.1:8086", "root", "root");
 
-        Query query = new Query("SELECT usage_idle FROM cpu LIMIT 10", "telegraf");
+  public static void main(String[] args) throws IOException {
+    InfluxDB influxDB = InfluxDBFactory.connect("http://127.0.0.1:8086", "root", "root");
 
-        URL url=new URL("http://127.0.0.1:8086/query?db=telegraf&q=SELECT%20usage_idle%20FROM%20cpu%20LIMIT%2020&pretty=true");
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setRequestProperty("Accept","application/csv"); // -H Accept:application/csv //format=csv
+    Query query = new Query("SELECT usage_idle FROM cpu LIMIT 10", "telegraf");
 
-        try(BufferedReader reader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
-            StringBuilder stringBuilder=new StringBuilder();
-            String line;
-            while ((line=reader.readLine())!=null){
-                stringBuilder.append(line).append("\n");
-            }
-            System.out.println(stringBuilder);
-            urlConnection.disconnect();
-        }
+    URL url = new URL(
+        "http://127.0.0.1:8086/query?db=telegraf&q=SELECT%20usage_idle%20FROM%20cpu%20LIMIT%2020&pretty=true");
+    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    urlConnection.setRequestMethod("GET");
+    urlConnection
+        .setRequestProperty("Accept", "application/csv"); // -H Accept:application/csv //format=csv
 
+    try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(urlConnection.getInputStream()))) {
+      StringBuilder stringBuilder = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        stringBuilder.append(line).append("\n");
+      }
+      System.out.println(stringBuilder);
+      urlConnection.disconnect();
+    }
 
-        QueryResult result = influxDB.query(query);
-        System.out.println(query.getCommandWithUrlEncoded());
-        System.out.println(result.toString());
+    QueryResult result = influxDB.query(query);
+    System.out.println(query.getCommandWithUrlEncoded());
+    System.out.println(result.toString());
 //        influxDB.deleteDatabase(dbName);
 
-    }
+  }
 }
