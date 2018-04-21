3030e1d0e542225bcfa4424a12c633ba8c4b7116
Nikolai Bystrov <nikolai_bystrov@epam.com>
2018-04-21 22:52:08 +0300

    added credentials utils

---
+++ Postgres/src/main/java/com/abra/timeseries/utils/CredentialsUtils.java
@@ -0,0 +1,24 @@
+package com.abra.timeseries.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class CredentialsUtils {
+
+  public static final String DB_CREDENTIALS_ENV = "./db_credentials.env";
+
+  public static Properties getPropFromEnvFile() {
+    Properties properties = new Properties();
+    try {
+      properties.load(new FileInputStream(DB_CREDENTIALS_ENV));
+      Properties result = new Properties();
+      result.setProperty("user", properties.getProperty("POSTGRES_USER"));
+      result.setProperty("password", properties.getProperty("POSTGRES_PASSWORD"));
+      return result;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
--- Postgres/src/test/java/PostgresJdbcTest.java
+++ Postgres/src/test/java/PostgresJdbcTest.java
@@ -1,19 +1,41 @@
+import com.abra.timeseries.utils.CredentialsUtils;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PostgresJdbcTest {
 
+  public static final String DB_CREDENTIALS_ENV = "./db_credentials.env";
   private String URL = "jdbc:postgresql://localhost:5433/test";
 
   @Test
-  public void testJdbcConnection() throws ClassNotFoundException {
+  public void testPath() {
+    File file = new File(".");
+    System.out.println(file.getAbsolutePath());
+  }
+
+  @Test
+  public void testProperty() throws IOException {
+
+    System.out.println(CredentialsUtils.getPropFromEnvFile().keySet());
+  }
+
+  @Test
+  public void testJdbcConnection() throws ClassNotFoundException, IOException {
 
     Class.forName("org.postgresql.Driver");
-    try (Connection connection = DriverManager.getConnection(URL, "timeseries", "custom")) {
+
+    try (Connection connection = DriverManager
+        .getConnection(URL, CredentialsUtils.getPropFromEnvFile())) {
 //      String sqlCreateTable = "INSERT INTO test.schema.students(name, id_group) VALUES(?,?)";
       Statement statement = connection.createStatement();
       statement.executeUpdate("CREATE SCHEMA IF NOT EXISTS test");
@@ -31,6 +53,7 @@ public class PostgresJdbcTest {
 
       statement.executeUpdate("DROP SCHEMA test CASCADE ");
     } catch (SQLException e) {
+      Assert.fail();
       e.printStackTrace();
     }
   }
